2f83d6a9ebd1ee19a5ee630a666dfa6271081966
Casey Foster <c@sey.me>
2014-06-12 08:09:33 -0500

    Beef up test page

--- test.js
+++ test.js
@@ -15,41 +15,78 @@
     },
 
     handleChange: function (ev) {
-      this.update({$set: parseInt(ev.target.value) || 0});
+      var val = parseInt(ev.target.value) || 0;
+      if (this.getState() !== val) this.update({$set: val});
     },
 
     render: function () {
-      return React.DOM.input({
-        value: this.getState(),
-        onChange: this.handleChange
-      });
+      return React.DOM.div(null,
+        React.DOM.input({
+          value: this.getState(),
+          onChange: this.handleChange
+        }),
+        React.DOM.input({
+          type: 'button',
+          onClick: this.props.onRemove,
+          value: 'Remove'
+        })
+      );
     }
   });
 
   var NumberList = React.createClass({
     mixins: [Curator],
 
+    handleAdd: function () {
+      this.update({$push: [0]});
+    },
+
+    handleRemove: function (i) {
+      this.update({$splice: [[i, 1]]});
+    },
+
     renderNumber: function (n, i) {
       return NumberListItem({
+        key: i,
         curator: this.getCurator(),
-        cursor: this.getCursor(i)
+        cursor: this.getCursor(i),
+        onRemove: this.handleRemove.bind(this, i)
       });
     },
 
     render: function () {
-      return React.DOM.div(null, this.getState().map(this.renderNumber));
+      return React.DOM.div(null,
+        this.getState().map(this.renderNumber),
+        React.DOM.input({
+          type: 'button',
+          onClick: this.handleAdd,
+          value: 'Add New Number'
+        })
+      );
     }
   });
 
   var Stats = React.createClass({
     mixins: [Curator],
 
+    getCardinality: function () {
+      return this.getState().length;
+    },
+
     getSum: function () {
       return this.getState().reduce(function (sum, n) { return sum + n; }, 0);
     },
 
+    getMean: function () {
+      return this.getSum() / this.getCardinality();
+    },
+
     render: function () {
-      return React.DOM.div(null, 'Sum: ' + this.getSum());
+      return React.DOM.div(null,
+        React.DOM.div(null, 'Cardinality: ' + this.getCardinality()),
+        React.DOM.div(null, 'Sum: ' + this.getSum()),
+        React.DOM.div(null, 'Mean: ' + this.getMean())
+      );
     }
   });
 
@@ -62,9 +99,46 @@
       };
     },
 
+    componentWillMount: function () {
+      this.undo = [];
+      this.redo = [];
+    },
+
+    componentWillUpdate: function () {
+      if (this.timeTraveling) return this.timeTraveling = false;
+      this.undo.push(this.state);
+      this.redo = [];
+    },
+
+    handleUndo: function () {
+      if (!this.undo.length) return;
+      this.timeTraveling = true;
+      this.redo.push(this.state);
+      this.setState(this.undo.pop());
+    },
+
+    handleRedo: function () {
+      if (!this.redo.length) return;
+      this.timeTraveling = true;
+      this.undo.push(this.state);
+      this.setState(this.redo.pop());
+    },
+
     render: function () {
       return (
-        React.DOM.div(null, [
+        React.DOM.div(null,
+          React.DOM.input({
+            type: 'button',
+            onClick: this.handleUndo,
+            value: 'Undo',
+            disabled: this.undo.length === 0
+          }),
+          React.DOM.input({
+            type: 'button',
+            onClick: this.handleRedo,
+            value: 'Redo',
+            disabled: this.redo.length === 0
+          }),
           NumberList({
             curator: this.getCurator(),
             cursor: this.getCursor('numbers')
@@ -73,7 +147,7 @@
             curator: this.getCurator(),
             cursor: this.getCursor('numbers')
           })
-        ])
+        )
       );
     }
   });
